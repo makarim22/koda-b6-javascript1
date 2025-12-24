f42c273eb3a49520f3c8725ff44aa5b33309f51b
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-24 10:45:34 +0700

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,13 @@
+// menghitung luas dan keliling lingkaran
+
+const phi = 3.14;
+
+let r = 20
+
+let luasLingkaran = phi * r * r;
+
+console.log("luas lingkaran adalah " + luasLingkaran);
+
+let kelilingLingkaran = 2 * phi * r;
+
+console.log("keliling lingkarang adalah " + kelilingLingkaran);
