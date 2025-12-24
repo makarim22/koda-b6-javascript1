1a8654b47501ee39a5f587532ce0ba517ce4f2f9
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-24 15:21:51 +0700

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,16 +1,24 @@
 // menghitung luas dan keliling lingkaran
 
-const phi = 3.14;
+let jariLingkaran = 14;
 
-let jariLingkaran = 21;
+let phi = null;
+if (jariLingkaran % 7 == 0){
+  phi = 22/7;
+} else {
+  phi = 3.14;
+} 
 
-let luasLingkaran = phi * r * r;
+console.log("phi :", phi);
 
-let kelilingLingkaran = 2 * phi * r;
+let luasLingkaran = phi * jariLingkaran * jariLingkaran;
+
+let kelilingLingkaran = 2 * phi * jariLingkaran;
 
 console.log("keliling lingkarang adalah " + kelilingLingkaran);
 
 console.log("luas lingkaran adalah " + luasLingkaran);
+
 
 
 
