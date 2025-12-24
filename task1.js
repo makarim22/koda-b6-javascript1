65f90af4083c0ba7624787164130efd8a86ef70b
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-24 11:23:35 +0700

    Update task1.js

--- task1.js
+++ task1.js
@@ -4,13 +4,6 @@ const phi = 3.14;
 
 let r = 21;
 
-let phi = null;
-if (r % 7 == 0) {
-    phi = 22 / 7;
-} else {
-    phi = 3.14;
-}
-
 let luasLingkaran = phi * r * r;
 
 console.log("luas lingkaran adalah " + luasLingkaran);
@@ -18,6 +11,7 @@ console.log("luas lingkaran adalah " + luasLingkaran);
 let kelilingLingkaran = 2 * phi * r;
 
 console.log("keliling lingkarang adalah " + kelilingLingkaran);
+
 
 
 
